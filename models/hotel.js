2957c206261b75aefbd167a6e9415596b2e44831
DhaniCuPliZ <ramadhanirafli065@gmail.com>
2025-11-05 10:48:11 +0700

    first commit

---
+++ models/hotel.js
@@ -0,0 +1,35 @@
+module.exports = (sequelize, DataTypes) => {
+  const Hotel = sequelize.define('hotel', {
+    id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    Tipe_Kamar: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    Kapasitas_Tamu: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    Lantai: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    Fasilitas: {
+        type: DataTypes.TEXT,
+        allowNull: false
+    },
+    Tanggal_Pesan: {
+        type: DataTypes.DATE,
+        allowNull: false
+    }
+
+}, {
+    tableName: 'hotel',
+    freezeTableName: true,
+    timestamps: true
+});
+    return Hotel;
+};
